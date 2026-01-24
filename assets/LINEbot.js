2a6b65688bd842d845c138b1f0c832e92948b2b2
david123666 <43198592+david123666@users.noreply.github.com>
2026-01-24 11:12:35 +0800

    Update LINEbot.js

--- assets/LINEbot.js
+++ assets/LINEbot.js
@@ -4,15 +4,15 @@
         liff.login();
       } else {
         const profile = await liff.getProfile();
-        const idToken = liff.getIDToken(); // 取得 ID Token
+        // const idToken = liff.getIDToken(); // 取得 ID Token
         // 傳送給 n8n
         await fetch("https://lumpier-odessa-distinguishingly.ngrok-free.dev/webhook/skin-analysis", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
             userId: profile.userId,
-            displayName: profile.displayName,
-            idToken: idToken 
+            displayName: profile.displayName
+            // idToken: idToken 
           })
         });
         // alert("資料已傳送至 n8n！");
